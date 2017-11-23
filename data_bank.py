3d56aa71080e8450d3c8b0d1642f4a7f39fb1279
Houchw <houchw@163.com>
2017-11-23 21:33:58 +0800

    modify the path of the db file

--- data_bank.py
+++ data_bank.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8-*-
 # import all the packages that are needed
 
-import os
+import sys
 import sqlite3
 #import pandas as pd
 import dash
@@ -115,7 +115,8 @@ def update_table(n_clicks, received_reviewed, app_type, drug_type, app_no, drug_
     # in this database, there are three tables named 'ims_sales', 'drugs_in_review',
     # 'drugs_appl_received'
 
-    db_path = os.getcwd() + "/data/drugsatcde.db"
+    db_path = sys.path[0] + "/data/drugsatcde.db"
+    #print(db_path)
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     results = cursor.execute(query)
